847fdb9c1744ed5b89aaebfe88cefe539152f81d
pablo <pablo_mix_16@hotmail.com>
2023-01-19 18:50:26 -0300

    pablo

--- api/src/PokemonsHandlers/PokemonsHandlers.js
+++ api/src/PokemonsHandlers/PokemonsHandlers.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+const { json } = require("sequelize");
+
 const {
   createPokemon,
   getPokemonsById,
@@ -17,15 +20,78 @@ const getPokemons = async (req, res) => {
 };
 
 const getPokemonsID = async (req, res) => {
-  const { id } = req.params;
-  const fuente = isNaN(id) ? "bdd" : "api";
-
   try {
-    const pokemonID = await getPokemonsById(id, fuente);
-    res.status(200).json(pokemonID);
+    const { id } = req.params;
+
+    let pokemon;
+    if (isNaN(id)) {
+      // Buscamos el Pokémon en la base de datos
+      pokemon = await Pokemon.findOne({
+        where: { id },
+        include: [
+          {
+            model: Types,
+            attributes: ["name"],
+          },
+        ],
+      });
+      // si no se encuentra en la base de datos
+      if (!pokemon) {
+        res.status(404).json({ message: "No se Encontro el Pokemon" });
+      } else {
+        // si se encuentra en la base de datos
+        pokemon = {
+          id: pokemon.id,
+          name: pokemon.name,
+          hp: pokemon.hp,
+          attack: pokemon.attack,
+          defense: pokemon.defense,
+          speed: pokemon.speed,
+          height: pokemon.height,
+          weight: pokemon.weight,
+          image: pokemon.image,
+          types: pokemon.types.map((el) => el.name),
+        };
+        res.json(pokemon);
+      }
+    } else {
+      // si es un numero busca en la api
+      const apiPokemon = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      // Procesamos la respuesta de la API
+      pokemon = {
+        id: apiPokemon.data.id,
+        name: apiPokemon.data.name,
+        hp: apiPokemon.data.stats[0].base_stat,
+        attack: apiPokemon.data.stats[1].base_stat,
+        defense: apiPokemon.data.stats[2].base_stat,
+        speed: apiPokemon.data.stats[5].base_stat,
+        height: apiPokemon.data.height,
+        weight: apiPokemon.data.weight,
+        image: apiPokemon.data.sprites.other.home.front_default,
+        types: apiPokemon.data.types.map((el) => el.type.name),
+      };
+      // Validamos si el Pokémon existe
+      if (!pokemon) {
+        res.status(404).json({ message: "Pokemon not found" });
+      } else {
+        // Respondemos al cliente con el Pokémon
+        res.json(pokemon);
+      }
+    }
   } catch (error) {
-    res.status(400).json({ error: error.mesage });
+    res.send(400).json({ error: error.mesage });
   }
+
+  //   const { id } = req.params;
+  //   const fuente = isNaN(id) ? "bdd" : "api";
+  //   try {
+  //     const pokemonID = await getPokemonsById(id, fuente);
+  //     res.status(200).json(pokemonID);
+  //   } catch (error) {
+  //     res.status(400).json({ error: error.mesage });
+  //   }
 };
 
 const postPokemons = async (req, res) => {
--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -1,13 +1,69 @@
 const { Pokemon, Type } = require("../db");
 const axios = require("axios");
 
-getPokemonsById = async (id, fuente) => {
-  const pokemonID =
-    fuente === "api"
-      ? (await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)).data
-      : await Pokemon.findByPk(id, { include: Type });
+getPokemonsById = async (id) => {
+  // const pokemonID =
+  //   fuente === "api"
+  //     ? (await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)).data
+  //     : await Pokemon.findByPk(id, { include: Type });
 
-  return pokemonID;
+  // return pokemonID;
+
+  let pokemonID;
+
+  if (isNaN(id)) {
+    pokemonID = await Pokemon.findOne({
+      where: { id },
+      include: [
+        {
+          model: Types,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    if (!pokemonID) {
+      res.status(404).json({ message: "No se Encontro el Pokemon" });
+    } else {
+      pokemonID = {
+        id: pokemon.id,
+        name: pokemon.name,
+        hp: pokemon.hp,
+        attack: pokemon.attack,
+        defense: pokemon.defense,
+        speed: pokemon.speed,
+        height: pokemon.height,
+        weight: pokemon.weight,
+        image: pokemon.image,
+        types: pokemon.types.map((el) => el.name),
+      };
+      res.json(pokemonID);
+    }
+  } else {
+    const apiPokemon = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${id}`
+    );
+    // Procesamos la respuesta de la API
+    pokemonID = {
+      id: apiPokemon.data.id,
+      name: apiPokemon.data.name,
+      hp: apiPokemon.data.stats[0].base_stat,
+      attack: apiPokemon.data.stats[1].base_stat,
+      defense: apiPokemon.data.stats[2].base_stat,
+      speed: apiPokemon.data.stats[5].base_stat,
+      height: apiPokemon.data.height,
+      weight: apiPokemon.data.weight,
+      image: apiPokemon.data.sprites.other.home.front_default,
+      types: apiPokemon.data.types.map((el) => el.type.name),
+    };
+    // Validamos si el Pokémon existe
+    if (!pokemonID) {
+      res.status(404).json({ message: "Pokemon not found" });
+    } else {
+      // Respondemos al cliente con el Pokémon
+      res.json(pokemonID);
+    }
+  }
 };
 
 const createPokemon = async (
